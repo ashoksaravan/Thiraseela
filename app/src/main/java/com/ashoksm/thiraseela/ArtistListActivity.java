a7f08a49a88cd4fc15c8ea33c540754bf9325b5b
Ashok Saravanan M <mani.ashoksaravanan@gmail.com>
2015-03-19 09:19:05 +0530

    Committed the lasted changes asked by Kiran.

--- app/src/main/java/com/ashoksm/thiraseela/ArtistListActivity.java
+++ app/src/main/java/com/ashoksm/thiraseela/ArtistListActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 import com.ashoksm.thiraseela.adapter.ArtistListAdapter;
 import com.ashoksm.thiraseela.vo.ArtistListVO;
